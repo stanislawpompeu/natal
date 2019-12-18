370edeb8cd8b7ba4eaa654bb9e6bb0acffc7379b
JoaoPedroPP <jp052@hotmail.com>
2019-12-18 11:52:24 -0300

    first commit

---
+++ build/precache-manifest.ecd8f14e76661c1340d0648a22981f8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e1e882a031adb21210681d9fe172269",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd739667d20f06832a0c",
+    "url": "/static/css/main.edff7118.chunk.css"
+  },
+  {
+    "revision": "dae305984974f28e691d",
+    "url": "/static/js/2.f1bfbdc2.chunk.js"
+  },
+  {
+    "revision": "e0555b884937a95c4693c1b80d749ff5",
+    "url": "/static/js/2.f1bfbdc2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bd739667d20f06832a0c",
+    "url": "/static/js/main.9b22731f.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
